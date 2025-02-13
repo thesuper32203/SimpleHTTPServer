081a116956ee0e81e1580cdc900cb2d578dcb923
Anthony <thesuper2021@gmail.com>
2025-02-13 18:10:45 -0500

    added SeverSocket sever

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.awt.desktop.SystemEventListener;
+import java.net.ServerSocket;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception {
+        final ServerSocket  sever = new ServerSocket(8080);
+
+        System.out.println("Listening for connection on port 8080");
+        while(true){
+
+        }
     }
 }
