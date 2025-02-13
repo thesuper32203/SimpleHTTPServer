adc9e3c2de48893b68b582f183ca4e7c744f1a69
Anthony <thesuper2021@gmail.com>
2025-02-13 18:38:36 -0500

    accept and read http sever request

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 import java.awt.desktop.SystemEventListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -7,7 +11,30 @@ public class Main {
 
         System.out.println("Listening for connection on port 8080");
         while(true){
+        final Socket client = sever.accept(); // blocking method blocks until a client connects to the server
+            // 1. Read HTTP request from the client socket
+            // 2. Prepare an HTTP response
+            // 3. Send HTTP response to the client
+            // 4. Close the socket
+            InputStreamReader isr
+                    = new InputStreamReader(client.getInputStream());
+            BufferedReader reader = new BufferedReader(isr);
+            String line = reader.readLine();
+            try {
+            while(!line.isEmpty()){
+
+                    System.out.println(line);
+                    line = reader.readLine();
+
+            }
+            } catch (NullPointerException e) {
+                System.out.println("Read all lines");
+            }
+
 
         }
+
+
+
     }
 }
